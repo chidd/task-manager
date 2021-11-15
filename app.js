f60d306c5d56fe732811f116327ea23e97ceb327
chinedu <chi.adams54@gmail.com>
2021-11-15 19:47:57 +0100

    Refactored the controllers for custom error handling and also plugged in the front end

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const app = express()
 const dotenv = require('dotenv')
 const tasks = require('./routes/tasks')
 const connectDB = require('./config/db')
-
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 // load env vars
 dotenv.config({path: './config/config.env'});
 
+
+app.use(express.static('./public'))
 app.use(express.json());
 
 connectDB()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/api/v1/tasks',tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 app.listen(port, ()=>{
     console.log(`server running on port ${port}...`)
